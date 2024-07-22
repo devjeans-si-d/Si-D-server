21cfa729556395feff6525392a1c15a3cc44fab0
clean2001 <clearrworld@gmail.com>
2024-07-22 15:23:01 +0900

    feat: repository 생성

---
+++ src/main/java/org/devjeans/sid/domain/launchedProject/repository/LaunchedProjectScrapRepository.java
@@ -0,0 +1,7 @@
+package org.devjeans.sid.domain.launchedProject.repository;
+
+import org.devjeans.sid.domain.launchedProject.entity.LaunchedProjectScrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaunchedProjectScrapRepository extends JpaRepository<LaunchedProjectScrap, Long> {
+}
