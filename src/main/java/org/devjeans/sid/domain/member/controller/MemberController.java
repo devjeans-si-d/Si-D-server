8adf8cb10364fe7ba423708fe1272008ed156dd1
sseho <kindseho@naver.com>
2024-07-23 20:34:13 +0900

    feat: kakao api 구현

--- src/main/java/org/devjeans/sid/domain/member/controller/MemberController.java
+++ src/main/java/org/devjeans/sid/domain/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package org.devjeans.sid.domain.member.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.devjeans.sid.domain.member.dto.MemberInfoResponse;
 import org.devjeans.sid.domain.member.dto.UpdateMemberRequest;
 import org.devjeans.sid.domain.member.dto.UpdateMemberResponse;
+import org.devjeans.sid.domain.member.entity.KakaoRedirect;
+import org.devjeans.sid.domain.member.entity.Member;
 import org.devjeans.sid.domain.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,4 +46,18 @@ public class MemberController {
 
         return new ResponseEntity<>(updateMemberResponse, HttpStatus.OK);
     }
+
+    @GetMapping("auth/kakao/callback")
+    public void kakaoCallback(KakaoRedirect kakaoRedirect) throws JsonProcessingException {
+            Long kakaoId = memberService.login(kakaoRedirect);
+//            가입자 or 비가입자 체크해서 처리
+            Member originMember = memberService.getMemberByKakaoId(kakaoId);
+            if(originMember == null) {
+                memberService.createMember(kakaoId);
+            }
+
+//            로그인 처리
+
+    }
+
 }
