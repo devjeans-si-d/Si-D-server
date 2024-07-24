856080409b1b94ee7bb98545fe2f7394a7fb83aa
clean2001 <clearrworld@gmail.com>
2024-07-24 15:15:08 +0900

    chore: WebSocketConfig 생성

---
+++ src/main/java/org/devjeans/sid/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package org.devjeans.sid.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // queue로 클라이언트로 메시지 전달(sub)
+        config.enableSimpleBroker("/queue");
+        // @Controller 객체의 @MessageMapping 메서드로 라우팅, 클라이언트가 서버로 메시지 보낼 URL 접두사(pub)
+        config.setApplicationDestinationPrefixes("/send");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/chat") //web socket connection이 최초로 이루어지는 곳(handshake)
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
