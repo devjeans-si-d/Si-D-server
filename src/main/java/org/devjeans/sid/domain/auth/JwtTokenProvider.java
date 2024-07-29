b553d652b36bcf5d3fc84ef007be0e28be0f2f93
sseho <kindseho@naver.com>
2024-07-29 18:55:28 +0900

    refactor: kakao email 추가 및 리팩토링

--- src/main/java/org/devjeans/sid/domain/auth/JwtTokenProvider.java
+++ src/main/java/org/devjeans/sid/domain/auth/JwtTokenProvider.java
@@ -18,6 +18,7 @@ public class JwtTokenProvider {
     private String secretKey;
     @Value("${jwt.expiration}")
     private int expiration;
+    private static final Long expriationPeriod = 24*60*60*1000L;
 
     public String createToken(String id, String role) {
 //        Claims는 사용자 정보(페이로드 정보)
@@ -27,7 +28,7 @@ public class JwtTokenProvider {
         String token = Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now) // 생성시간                일수 시간 분 초 밀리초
-                .setExpiration(new Date(now.getTime()+expiration*24*60*60*1000L)) // 만료시간 30분
+                .setExpiration(new Date(now.getTime()+expiration*expriationPeriod)) // 만료시간 30분
                 .signWith(SignatureAlgorithm.HS256,secretKey)
                 .compact();
         return token;
