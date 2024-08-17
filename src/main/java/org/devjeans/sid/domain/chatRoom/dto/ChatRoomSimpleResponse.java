38ee727f1455334ee92a2d600b6d5bfed82ae4a6
Sejeong Kim <clearrworld@gmail.com>
2024-08-17 18:40:12 +0900

    fix: 채팅 리스트 응답값 수정

--- src/main/java/org/devjeans/sid/domain/chatRoom/dto/ChatRoomSimpleResponse.java
+++ src/main/java/org/devjeans/sid/domain/chatRoom/dto/ChatRoomSimpleResponse.java
@@ -3,6 +3,9 @@ package org.devjeans.sid.domain.chatRoom.dto;
 import lombok.*;
 import org.devjeans.sid.domain.chatRoom.entity.ChatRoom;
 import org.devjeans.sid.domain.member.entity.Member;
+import org.devjeans.sid.domain.project.entity.Project;
+
+import java.time.LocalDateTime;
 
 @Builder
 @AllArgsConstructor
@@ -10,12 +13,19 @@ import org.devjeans.sid.domain.member.entity.Member;
 @Getter
 public class ChatRoomSimpleResponse { // 밖에서 미리보기처럼 보이는 채팅방 1개의 정보
     private Long chatRoomId;
-    private Long projectId;
     private Long participantMemberId; // 상대방의 멤버 아이디
     private String participantNickName; // 상대방 이름
     private String participantProfileImageUrl; // 상대방 프로필 사진
     private Long unreadCount; // 읽었는지 여부
     private String unreadContent; // 채팅 내용
+
+    // 프로젝트 정보
+    private Long projectId;
+    private String projectName;
+    private String projectImage;
+    private String projectDescription;
+    private LocalDateTime deadline;
+    private String isClosed;
 
     public static ChatRoomSimpleResponse fromEntity(ChatRoom chatRoom, Long unreadCount, Member participant, String unreadContent) {
         return ChatRoomSimpleResponse.builder()
@@ -26,6 +36,11 @@ public class ChatRoomSimpleResponse { // 밖에서 미리보기처럼 보이는 
                 .participantProfileImageUrl(participant.getProfileImageUrl())
                 .unreadCount(unreadCount)
                 .unreadContent(unreadContent)
+                .projectName(chatRoom.getProject().getProjectName())
+                .projectImage(chatRoom.getProject().getImageUrl())
+                .projectDescription(chatRoom.getProject().getDescription())
+                .deadline(chatRoom.getProject().getDeadline())
+                .isClosed(chatRoom.getProject().getIsClosed())
                 .build();
     }
 
