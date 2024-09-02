14099bf951726756571dd270d532278c3f9196ad
Sejeong Kim <clearrworld@gmail.com>
2024-09-02 23:04:29 +0900

    chore: 주석 추가

--- src/main/java/org/devjeans/sid/domain/chatRoom/controller/SseController.java
+++ src/main/java/org/devjeans/sid/domain/chatRoom/controller/SseController.java
@@ -1,6 +1,7 @@
 package org.devjeans.sid.domain.chatRoom.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.devjeans.sid.domain.chatRoom.service.SseService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/sse")
 @RestController
@@ -15,6 +17,7 @@ public class SseController {
     private final SseService sseService;
     @GetMapping("/subscribe")
     public SseEmitter subscribe() {
+        log.info("line 28: subscribe!!!!");
         return sseService.subscribe();
     }
 
