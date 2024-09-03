0637680a7552d142d11cb4cff1111433e6c3f9fb
Sejeong Kim <clearrworld@gmail.com>
2024-09-03 20:58:05 +0900

    chore: cors

--- src/main/java/org/devjeans/sid/config/WebConfig.java
+++ src/main/java/org/devjeans/sid/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
-                .allowedOrigins("https://dev.si-d.site")
+                .allowedOrigins("https://dev.si-d.site", "http://localhost:8082")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 쿠키 인증 요청 허용
     }
